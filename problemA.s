b33f6b69845ed1360e28f6612b10ba0d96882cb3
ianrwan <ianwan012088@gmail.com>
2025-10-13 17:39:15 +0800

    problemA update

---
+++ problemA.s
@@ -0,0 +1,210 @@
+.data
+
+input_text: .string "Input:"
+encode_text: .string "Encode:"
+decode_text: .string "Decode:"
+new_line: .string "\n"
+
+.text
+main:
+    li s11, 0
+    li a0, 0x01 # test data
+test: 
+    jal ra, input_text_console
+ 
+    mv s0, a0
+    jal ra, uf8_decode
+    jal ra, decode_text_console
+    
+    mv s1, a0
+    jal ra, uf8_encode
+    jal ra, encode_text_console
+    
+    li t0, 4
+    addi s11, s11, 1
+    mv a0, s0
+    addi a0, a0, 47
+    ble s11, t0, test
+    jal zero, end
+    
+
+# clz function
+# a0 = x
+clz:
+    li t0, 32 # t0 = n = 32
+    li t1, 16 # t1 = c = 16
+    
+clz_while:
+    srl t2, a0, t1 # t2 = y = x >> c
+    beq t2, zero, clz_y_false # if y == 0 jump
+    sub t0, t0, t1 # t0 = n = n-c
+    mv a0, t2 # a0 = x = y
+
+clz_y_false:
+    srli t1, t1, 1 # t1 = c = c >> 1
+    bne t1, zero, clz_while # if t1 != 0 continue
+    sub a0, t0, a0 # a0 = n-x
+    jalr ra
+
+# uf8_decode function
+# a0 = f1
+
+uf8_decode:
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    
+    andi t0, a0, 0x0f # t0 = mantissa = f1 & 0x0f
+    srli t1, a0, 4 # t1 = exponent = f1 >> 4
+    
+    li t2, 15
+    sub t2, t2, t1 # t2 = 15 - exponent
+    li t3, 0x7fff
+    srl t2, t3, t2 # t2 = 0x7fff >> (15-exponent)
+    slli t2, t2, 4 # t2 = offset = 0x7fff >> (15-exponent) << 4
+    
+    sll t0, t0, t1 # t0 = mantissa << exponent
+    add a0, t0, t2 # a0 = (mantissa << exponent) + offset
+    
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    jalr ra
+
+# uf8_encode_function
+# a0 = value
+uf8_encode:
+    addi sp, sp, -20
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    
+    mv s0, a0 # s0 = a0
+    
+    li t0, 16 # t0 = 16
+    bge a0, t0, uf8_encode_value_bge_16 # if value >= 16 jump
+    jal zero, uf8_encode_return # return value (a0 doesn't change, a0 is value) 
+
+uf8_encode_value_bge_16:
+    jal ra, clz # clz(value) (a0 doesn't change, a0 is value)
+    mv t0, a0 # t0 = lz = clz(value) (a0 change to clz(value))
+    li t1, 31 # t1 = 31
+    sub s1, t1, t0 # s1 = msb = 31-lz
+    
+    li s2, 0 # s2 = exponent = 0
+    li s3, 0 # s3 = overflow = 0
+    
+    li t0, 5 # t0 = 5
+    ble s1, t0, uf8_encode_while_find_exact_exponent # if msb < 5 jump
+    
+    addi s2, s1, -4 # s2 = exponent = msb-4
+    li t0, 16 # t0 = 16
+    ble s2, t0, uf8_encode_for_overflow_init # if exponent < 16 jump (exponent <= 15)
+    
+    li s2, 15 # s2 = exponent = 15
+
+uf8_encode_for_overflow_init:
+    li t0, 0 # t0 = e = 0
+uf8_encode_for_overflow:
+    bge t0, s2, uf8_encode_while_adjust # if e >= exponent jump
+    addi t0, t0, 1 # t0 = e = e + 1
+    
+    slli s3, s3, 1 # s3 = overflow << 1
+    addi s3, s3, 16 # s3 = s3 + 16
+    jal zero, uf8_encode_for_overflow # continue uf8_encode_for_overflow
+
+uf8_encode_while_adjust:
+    li t0, 1 # t0 = 1
+    ble s2, t0, uf8_encode_while_find_exact_exponent # if exponent < 1 jump (exponent <= 0)
+    bge s0, s3, uf8_encode_while_find_exact_exponent # if value >= overflow jump
+    
+    addi s3, s3, -16 # s3 = overflow - 16
+    srli s3, s3, 1 # s3 = (overflow - 16) >> 1
+    addi s2, s2, -1 # s2 = exponent = exponent - 1
+    jal zero, uf8_encode_while_adjust # continue uf8_encode_while_adjust
+
+uf8_encode_while_find_exact_exponent:
+    li t0, 15 # t0 = 15
+    bge s2, t0, uf8_encode_return # if exponent >= 15 jump
+    slli t0, s3, 1 # t0 = next_overflow =(overflow << 1)
+    addi t0, t0, 16 # t0 = next_overflow = (overflow << 1 )+16
+    
+    ble a0, t0, uf8_encode_return # if value < next_overflow break
+    mv s3, t0 # s3 = overflow = next_overflow
+    
+    addi s2, s2, 1 # s2 = exponent = exponent+1
+    jal zero, uf8_encode_while_find_exact_exponent  
+    
+uf8_encode_return:
+    sub t0, s0, s3 # t0 = mantissa = (value-overflow)
+    srl t0, t0, s2 # t0 = mantissa = (value-overflow) >> exponent
+    
+    slli a0, s2, 4 # a0 = exponent << 4
+    or a0, a0, t0 # a0 = (exponent) << 4
+    
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    addi sp, sp, 20
+    jalr ra
+    
+input_text_console:
+    mv t0, a0
+    
+    la a0, input_text
+    li a7, 4
+    ecall
+    
+    mv a0, t0
+    li a7, 1
+    ecall
+    
+    la a0, new_line
+    li a7, 4
+    ecall
+    
+    mv a0, t0
+    jalr ra
+    
+encode_text_console:
+    mv t0, a0
+    la a0, encode_text
+    li a7, 4
+    ecall
+    
+    mv a0, t0
+    li a7, 1
+    ecall
+    
+    la a0, new_line
+    li a7, 4
+    ecall
+    
+    la a0, new_line
+    li a7, 4
+    ecall
+    
+    mv a0, t0
+    jalr ra
+    
+decode_text_console:
+    mv t0, a0
+    la a0, decode_text
+    li a7, 4
+    ecall
+    
+    mv a0, t0
+    li a7, 1
+    ecall
+    
+    la a0, new_line
+    li a7, 4
+    ecall
+    
+    mv a0, t0
+    jalr ra
+
+end:
+    add zero, zero, zero
